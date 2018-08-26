f395e55919eda50bb91dc1b867dd373e064a695b
Heliovic <1005031096@qq.com>
2018-08-26 17:12:06 +0800

    1013 (21/25)
    无需检查是否还有未访问结点，去掉的结点直接 visit 标 1 即可。【测试点 4】运行超时

--- 1013/Main.java
+++ 1013/Main.java
@@ -23,32 +23,19 @@ public class Main {
 			concern[i] = scanner.nextInt();
 		}
 		for (int c : concern) {
-			int[][] g = new int[N+1][N+1];
-			for (int i = 0; i <= N; i++) {
-				for (int j = 0; j <= N; j++) {
-					g[i][j] = graph[i][j];
-				}
-			}
-			for (int i = 0; i <= N; i++) {
-				g[c][i] = 0;
-				g[i][c] = 0;
-			}
-			
 			int[] visit = new int[N+1];
 			for (int i = 0; i <= N; i++) {
 				visit[i] = 0;
 			}
-			int component = 0;
-			while (hasUnvisited(visit, N)) {
-				for (int i = 1; i <= N; i++) {
-					if (visit[i] == 0) {
-						component++;
-						dfs(i, g, visit, N);
-						break;
-					}
+			visit[c] = 1;
+			int component = 0;	// 连通分量数目
+			for (int i = 1; i <= N; i++) {
+				if (visit[i] == 0) {
+					component++;
+					dfs(i, graph, visit, N);
 				}
 			}
-			System.out.println(component - 1 - 1);
+			System.out.println(component - 1);	// 需要加入的边 = 连通分量数目 - 1
 		}
 	}
 	
@@ -59,13 +46,5 @@ public class Main {
 				dfs(i, g, visit, N);
 			}
 		}
-	}
-	
-	public static boolean hasUnvisited(int[] visit, int N) {
-		for (int i = 1; i <= N; i++) {
-			if (visit[i] == 0)
-				return true;
-		}
-		return false;
 	}
 }
