c1a162b7a776d796f7f17b7b25c9949ab0049fec
Heliovic <1005031096@qq.com>
2018-08-27 12:00:14 +0800

    1015 (20/20)
    质数判断

--- 1015/Main.java
+++ 1015/Main.java
@@ -9,30 +9,25 @@ public class Main {
 		int N, D;
 		while ((N = scanner.nextInt()) > 0) {
 			D = scanner.nextInt();
-			boolean isPrime = true;
-			for (int i = 2; i * i <= N; i++) {
-				if (N % i == 0) {
-					isPrime = false;
-					break;
-				}
-			}
-			if (!isPrime || N == 1) {
+			if (!isPrime(N)) {
 				answer.add("No");
 				continue;
 			}
 			String NS = Integer.toString(N, D);
 			NS = new StringBuilder(NS).reverse().toString();
 			int NR = Integer.parseInt(NS, D);
-			isPrime = true;
-			for (int i = 2; i * i <= NR; i++) {
-				if (NR % i == 0) {
-					isPrime = false;
-					break;
-				}
-			}
-			answer.add(isPrime && N != 1 ? "Yes" : "No");
+			answer.add(isPrime(NR) ? "Yes" : "No");
 		}
 		for (String a : answer)
 			System.out.println(a);
+	}
+	
+	public static boolean isPrime(int n) {
+		if (n < 2)
+			return false;
+		for (int i = 2; i * i <= n; i++)
+			if (n % i == 0)
+				return false;
+		return true;
 	}
 }
